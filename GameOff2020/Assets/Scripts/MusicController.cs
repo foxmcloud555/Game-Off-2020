ef6284c8bad322e57f843d58e9b49b356b7b817c
foxmcloud555 <katie.fisher555@gmail.com>
2020-11-30 23:15:01 +0000

    Music.

--- GameOff2020/Assets/Scripts/MusicController.cs
+++ GameOff2020/Assets/Scripts/MusicController.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicController : MonoBehaviour
 {
 
+    
+
+  
+
     void Awake()
     {
+        
        DontDestroyOnLoad(this.gameObject);
 
     }
 
     void Update()
     {
-        
+      
     }
-}
-
 }
