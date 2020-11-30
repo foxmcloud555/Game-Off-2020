7d70d15122c68f0f43b5336187964fed3858e7a3
foxmcloud555 <katie.fisher555@gmail.com>
2020-11-30 17:04:52 +0000

    Forbidden scene.

---
+++ GameOff2020/Assets/Scripts/ActProgresser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Controllers;
+
+public class ActProgresser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.CurrentAct = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
